7e8e4ce7bd87d2ca2d321428074e2a5ece49dd90
Erik Loebel <erik.loebel@tu-dresden.de>
2023-03-17 15:07:07 +0100

    Add files via upload

---
+++ GLACIER_FRONT_EXTRACTION.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python -u
+#
+# Purpose: Glacier calving front extraction from optical landsat-8 and 9 imagery
+# Author: Erik Loebel
+# Last change: 2023-02-02
+#
+#
+# ------------------------------------------
+
+import sys
+import subprocess
+
+glacier = str(sys.argv[1])
+if glacier == 'custom':
+	print("  - custom glacier: loading lon, lat coordinates", flush=True)
+	lon=str(sys.argv[2])
+	lat=str(sys.argv[3])
+
+### pre processing for all scenes in folder /input ###
+print('+++ INITIALIZING PRE-PROCESSING +++', flush=True)
+if glacier == 'zachariae_isstrom':
+	print("  - glacier is zachariae_isstrom ---> 2 seperate (and overlapping) ANN predictions neccessary", flush=True)
+	subprocess.call(["scripts/pre-processing.sct", 'zachariae_isstrom_north'])
+	subprocess.call(["scripts/pre-processing.sct", 'zachariae_isstrom_south'])
+elif glacier == 'nioghalvfjerdsbrae':
+	print("  - glacier is nioghalvfjerdsbrae ---> 3 seperate (and overlapping) ANN predictions neccessary", flush=True)
+	subprocess.call(["scripts/pre-processing.sct", 'nioghalvfjerdsbrae_a'])
+	subprocess.call(["scripts/pre-processing.sct", 'nioghalvfjerdsbrae_b'])
+	subprocess.call(["scripts/pre-processing.sct", 'nioghalvfjerdsbrae_c'])
+elif glacier == 'humboldt':
+	print("  - glacier is humboldt ---> 7 seperate (and overlapping) ANN predictions neccessary", flush=True)
+	subprocess.call(["scripts/pre-processing.sct", 'humboldt_a'])
+	subprocess.call(["scripts/pre-processing.sct", 'humboldt_c'])
+	subprocess.call(["scripts/pre-processing.sct", 'humboldt_e'])
+	subprocess.call(["scripts/pre-processing.sct", 'humboldt_g'])
+	subprocess.call(["scripts/pre-processing.sct", 'humboldt_i'])
+	subprocess.call(["scripts/pre-processing.sct", 'humboldt_k'])
+	subprocess.call(["scripts/pre-processing.sct", 'humboldt_m'])
+else:
+	if glacier == 'custom':
+		subprocess.call(["scripts/pre-processing.sct", glacier,lon,lat])
+	else:
+		subprocess.call(["scripts/pre-processing.sct", glacier])
+print("  - pre processing finished", flush=True)
+
+### Predicting calving front from pre processed data ###
+print('+++ INITIALIZING ANN PROCESSING +++', flush=True)
+subprocess.call(['python', 'scripts/predict_calving_front.py'])
