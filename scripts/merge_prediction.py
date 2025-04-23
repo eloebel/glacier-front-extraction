7ffedd119cd4c9ced8fa9b15a3d35e38afe82e18
Erik Loebel <erik.loebel@tu-dresden.de>
2025-04-23 20:40:11 +0200

    Update merge_prediction.py

--- scripts/merge_prediction.py
+++ scripts/merge_prediction.py
@@ -65,3 +65,21 @@ if GLACIER_NUMBER == 3:
 		temp_folder=OUTPUT_PATH + '/nioghalvfjerdsbrae/' + ids[n] + '/TEMP'
 		# GDAL operation include merging + vectorization
 		proc_gdal = subprocess.call(["scripts/gdal_operations_nioghalvfjerdsbrae.sct", temp_folder, pred_a, pred_b, pred_c, ids[n]])
+
+if GLACIER_NUMBER == 4:
+    Path(OUTPUT_PATH + '/hektoria-green-evans').mkdir(parents=True, exist_ok=True)      
+    ids = next(os.walk(OUTPUT_PATH + '/999.hektoria-green-evans_a'))[1]       # später: nur ids die in allen ordner sind
+   
+    for n in range(len(ids)):
+        print('      ... %s' % (ids[n]))
+        Path(OUTPUT_PATH + '/hektoria-green-evans/' + ids[n]).mkdir(parents=True, exist_ok=True)
+        Path(OUTPUT_PATH + '/hektoria-green-evans/' + ids[n] + '/TEMP').mkdir(parents=True, exist_ok=True)
+    
+        pred_a=OUTPUT_PATH + '/hektoria-green-evans_a/'+ ids[n] +'/pred_'+ ids[n] +'.tif'
+        pred_b=OUTPUT_PATH + '/hektoria-green-evans_b/'+ ids[n] +'/pred_'+ ids[n] +'.tif'
+        pred_c=OUTPUT_PATH + '/hektoria-green-evans_c/'+ ids[n] +'/pred_'+ ids[n] +'.tif'
+        pred_d=OUTPUT_PATH + '/hektoria-green-evans_d/'+ ids[n] +'/pred_'+ ids[n] +'.tif'
+        pred_e=OUTPUT_PATH + '/hektoria-green-evans_e/'+ ids[n] +'/pred_'+ ids[n] +'.tif'
+        temp_folder=OUTPUT_PATH + '/hektoria-green-evans/' + ids[n] + '/TEMP'
+        # GDAL operation include merging + vectorization
+	proc_gdal = subprocess.call(["scripts/gdal_operations_hektoria-green-evans.sct", temp_folder, pred_a, pred_b, pred_c, pred_d, pred_e,ids[n]])
